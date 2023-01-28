421edf8331232141dd8985c825f0600ea6459c0d
Siyatelev_Vladimir <siyatelev@yandex.ru>
2023-01-29 00:29:38 +0300

    Yello, Masha!!!

---
+++ Example005_HelloMasha/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Привет, назови себя");
+string username = Console.ReadLine();
+if(username.ToLower() == "маша")
+{
+Console.WriteLine("Ура, это Маруся!!!");
+}
+else
+{
+Console.Write("Привет, ");
+Console.WriteLine(username);
+}
