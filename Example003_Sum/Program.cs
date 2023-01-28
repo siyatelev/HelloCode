802f2d44e53c4f60cbe52125ec2584b53368fd03
Siyatelev_Vladimir <siyatelev@yandex.ru>
2023-01-29 00:13:45 +0300

    add sum randome

---
+++ Example003_Sum/Program.cs
@@ -0,0 +1,6 @@
+﻿int NumberA = new Random().Next(1,10); //1,2,3,4...
+Console.WriteLine(NumberA);
+int NumberB = new Random().Next(1,10);
+Console.WriteLine(NumberB);
+int result = NumberA + NumberB;
+Console.WriteLine(result);
