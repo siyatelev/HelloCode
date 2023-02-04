3aa6dec75a9b3613fddba1e06bd2d877d3c68d4c
Vladimir Siyatelev <siyatelev@yandex.ru>
2023-02-05 01:16:20 +0300

    add Example 012_Method lesson 3

--- Example012_Methods/Program.cs
+++ Example012_Methods/Program.cs
@@ -90,35 +90,121 @@ int year = Method3();
 // маленькие буквы "к" заминить большими "К",
 // а большие "С" заминить маленькими "с".
 
-string text = "– я думаю, – сказал князь, улыбаясь, - что,"
-            + "Ежели бы вас послали всемто нашего милого Винценгероде,"
-            + "Вы бы взяли приступом согласие прусского короля."
-            + "СССВы так красноречивы. Вы дадите мне чаю?";
+// string text = "– я думаю, – сказал князь, улыбаясь, - что,"
+//             + "Ежели бы вас послали всемто нашего милого Винценгероде,"
+//             + "Вы бы взяли приступом согласие прусского короля."
+//             + "СССВы так красноречивы. Вы дадите мне чаю?";
 
-// string s = "qwerty"
-// 012345
-//s[3] //r
+// // string s = "qwerty"
+// // 012345
+// //s[3] //r
 
-string Replace(string text, char oldValue, char newValue)
+// string Replace(string text, char oldValue, char newValue)
+// {
+//     string result = String.Empty;
+
+//     int length = text.Length;
+//     for (int i = 0; i < length; i++)
+//     {
+//         if(text[i] == oldValue) result = result + $"{newValue}";
+//         else result = result + $"{text[i]}";
+//     }
+//     return result;
+// }
+
+// string newText = Replace(text, ' ', '|');
+
+// Console.WriteLine(newText);
+// Console.WriteLine();
+// newText = Replace(newText, 'к', 'К');
+// Console.WriteLine(newText);
+// Console.WriteLine();
+// newText = Replace(newText, 'С', 'с');
+// Console.WriteLine(newText);
+// Console.WriteLine();
+
+
+// // //Задача. Сортировка массива. 
+// // 1. найти минимальный в неотсортированной части массива.
+// // 2. произвести обмент этого значения со значением первой неотсортированной позиции.
+// // 3. повторять пока есть неотсортированные элементы.
+
+// int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
+
+// void PrintArray(int[] array)
+// {
+//     int count = array.Length;
+
+//     for (int i = 0; i < count; i++)
+//     {
+//         Console.Write($"{array[i]} ");
+//     }
+//     Console.WriteLine();
+// }
+
+// void SelectionSort(int[] array)
+// {
+//     for (int i = 0; i < array.Length - 1; i++)
+//     {
+//         int minPosition = i;
+
+//         for (int j = i + 1; j < array.Length; j++)
+//         {
+//             if (array[j] < array[minPosition]) minPosition = j;
+//         }
+
+//         int temporary = array[i];
+//         array[i] = array[minPosition];
+//         array[minPosition] = temporary;
+
+//     }
+// }
+
+// PrintArray(arr);
+// SelectionSort(arr);
+
+// PrintArray(arr);
+// ///////////////
+
+
+// //Задача. Сортировка массива в обратном порядке. 
+// 1. найти максимальный в неотсортированной части массива.
+// 2. произвести обмент этого значения со значением первой неотсортированной позиции.
+// 3. повторять пока есть неотсортированные элементы.
+
+int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
+
+void PrintArray(int[] array)
 {
-    string result = String.Empty;
+    int count = array.Length;
 
-    int length = text.Length;
-    for (int i = 0; i < length; i++)
+    for (int i = 0; i < count; i++)
     {
-        if(text[i] == oldValue) result = result + $"{newValue}";
-        else result = result + $"{text[i]}";
+        Console.Write($"{array[i]} ");
     }
-    return result;
+    Console.WriteLine();
 }
 
-string newText = Replace(text, ' ', '|');
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int maxPosition = i;
 
-Console.WriteLine(newText);
-Console.WriteLine();
-newText = Replace(newText, 'к', 'К');
-Console.WriteLine(newText);
-Console.WriteLine();
-newText = Replace(newText, 'С', 'с');
-Console.WriteLine(newText);
-Console.WriteLine();
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] > array[maxPosition]) maxPosition = j;
+        }
+
+        int temporary = array[i];
+        array[i] = array[maxPosition];
+        array[maxPosition] = temporary;
+
+    }
+}
+
+PrintArray(arr);
+SelectionSort(arr);
+
+PrintArray(arr);
+///////////////
