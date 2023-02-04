22794dff8a4aabaa388569042914eee4f3df63cd
Vladimir Siyatelev <siyatelev@yandex.ru>
2023-02-05 00:36:24 +0300

    add Example012_Method

---
+++ Example012_Methods/Program.cs
@@ -0,0 +1,124 @@
+﻿// Вид 1
+void Method1()
+{
+    Console.WriteLine("Автор...");
+}
+//Method1();
+
+
+// Вид 2 (ничего не возвращают, но принимают какие-либо аргументы)
+void Method2(string msg)
+{
+    Console.WriteLine(msg);
+}
+// Method2(msg: "Текст сообщения");
+
+void Method21(string msg, int count)
+{
+    int i = 0;
+    while (i < count)
+    {
+        Console.WriteLine(msg);
+        i++;            //инкримент, уменьшение декримент
+    }
+}
+// Method21("Текст", 4);
+// Method21(count: 4, msg: "новый текст");
+
+
+
+// Вид 3 (ничего не принимают, но что-то возвращают,)
+
+
+int Method3()
+{
+    return DateTime.Now.Year;
+}
+int year = Method3();
+// Console.WriteLine(year);
+
+// Вид 4 (что-то принимают и что-то возвращают)
+
+// string Method4(int count, string text)
+// {
+//     int i = 0;
+//     string result = String.Empty;
+
+//     while (i < count)
+//     {
+//         result = result + text;
+//         i++;
+//     }
+//     return result;
+// }
+
+// string res = Method4(10, "asdf");
+// Console.WriteLine(res);
+
+
+
+
+////Цикл FOR
+
+// string Method4(int count, string text)
+// {
+//     string result = String.Empty;
+//     for (int i = 0; i < count; i++)
+//     {
+//         result = result + text;
+//     }
+//     return result;
+// }
+
+
+
+// string res = Method4(10, "asdf");
+// Console.WriteLine(res);
+
+// for (int i = 2; i <= 10; i++)
+// {
+//     for (int j = 2; j <= 10; j++)
+//     {
+//         Console.WriteLine($"{i} x {j} = { i* j}");    
+//     }
+//     Console.WriteLine();
+// }
+
+
+// // ===== Работа с текстом
+// Дан текст. В тексте нужно все пробелы заменить черточками, 
+// маленькие буквы "к" заминить большими "К",
+// а большие "С" заминить маленькими "с".
+
+string text = "– я думаю, – сказал князь, улыбаясь, - что,"
+            + "Ежели бы вас послали всемто нашего милого Винценгероде,"
+            + "Вы бы взяли приступом согласие прусского короля."
+            + "СССВы так красноречивы. Вы дадите мне чаю?";
+
+// string s = "qwerty"
+// 012345
+//s[3] //r
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if(text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+    return result;
+}
+
+string newText = Replace(text, ' ', '|');
+
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replace(newText, 'к', 'К');
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replace(newText, 'С', 'с');
+Console.WriteLine(newText);
+Console.WriteLine();
